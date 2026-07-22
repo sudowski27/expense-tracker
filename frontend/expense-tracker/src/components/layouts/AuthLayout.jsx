b31ff226627225e5eabf8295f53cd0b3c4f7a7dc
sudowski27 <Sudowski@o2.pl>
2026-07-23 00:23:53 +0200

    Update a file AuthLayout.jsx

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -4,6 +4,7 @@ import { LuTrendingUpDown } from "react-icons/lu";
 
 const AuthLayout = ({children}) => {
     return (
+        <div>
         <div className="flex">
             <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
                 <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
@@ -29,6 +30,7 @@ const AuthLayout = ({children}) => {
             />
         </div>
         </div>
+        </div>
     );
 };
 
@@ -37,7 +39,7 @@ export default AuthLayout;
 const StatsInfoCard = ({icon, label, value, color}) => {
     return <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10">
         <div
-            className={'w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl'}
+            className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}
             >
                 {icon}
             </div>
