2682028a20d9d76ce0c45274ece3be7dc393f467
sudowski27 <Sudowski@o2.pl>
2026-07-23 00:25:02 +0200

    Update the file /frontend/expense-tracker/src/pages/Auth/Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
     return (
         <AuthLayout>
-            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col juistify-center">
+            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
                 <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
                 <p className="text-xs text-slate-700 mt-[5px] mb-6">
                     Please enter your details to log in
