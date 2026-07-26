a7014cc6ddaf456a90af9b7b10f81b09b4e9c917
sudowski27 <Sudowski@o2.pl>
2026-07-26 20:50:56 +0200

    Update the file src/pages/Auth/Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from 'react-router-dom';
+import Input from "../../components/Inputs/Input";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    // Handle Logir for Submit
+    const handleLogin = async (e) => {}
+
+
     return (
         <AuthLayout>
             <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -12,6 +21,16 @@ const Login = () => {
                 <p className="text-xs text-slate-700 mt-[5px] mb-6">
                     Please enter your details to log in
                 </p>
+
+                <form onSubmit={handleLogin}>
+                    <Input
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)}
+                        label="Email Address"
+                        placeholder="john@example.com"
+                        type="text"
+                    />
+                </form>
             </div>
         </AuthLayout>
     )
