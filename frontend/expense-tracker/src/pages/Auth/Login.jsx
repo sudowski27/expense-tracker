8246db6b18557a633de84271eb96f1d56026643b
sudowski27 <Sudowski@o2.pl>
2026-07-28 00:30:16 +0200

    Update the file frontend/expense-tracker/src/pages/Auth/Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/Input";
 
 const Login = () => {
@@ -30,6 +30,27 @@ const Login = () => {
                         placeholder="john@example.com"
                         type="text"
                     />
+
+                    <Input
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                        label="Password"
+                        placeholder="Min 8 Characters"
+                        type="password"
+                    />
+
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+                    <button type="submit" className="btn-primary">
+                        LOGIN
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        Don't have an account?{" "}
+                        <Link className="font-medium text-primary underline" to="/signup">
+                        SignUp
+                        </Link>
+                    </p>
                 </form>
             </div>
         </AuthLayout>
