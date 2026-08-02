b06005fa49416535e9ad53bd7f8b4f2c1b63d57c
sudowski27 <Sudowski@o2.pl>
2026-08-02 13:54:34 +0200

    Add the ProfilePhotoSelector.jsx file

---
+++ frontend/expense-tracker/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProfilePhotoSelector = ({image, setImage}) => {
+    const inputRef = useRef(null);
+    return (
+        <div>ProfilePhotoSelector</div>
+    )
+}
+
+export default ProfilePhotoSelector
