eef6ba256f3a7ba6bb1f93337add7aa35f3490ab
sudowski27 <Sudowski@o2.pl>
2026-08-04 00:14:54 +0200

    Update the file frontend/expense-tracker/src/pages/Auth/SignUp.jsx

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -16,7 +16,30 @@ const SignUp = () => {
     const navigate = useNavigate();
 
     // Handle Sign Up Form Submit
-    const handleSignUp = async (e) => {}
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        let profileImageUrl = "";
+
+        if(!fullName) {
+            setError("Please enter your name");
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if(!password) {
+            setError("Please enter the password");
+            return;
+        }
+
+        setError("");
+
+        // SignUp API Call
+    }
 
 
     return (
@@ -57,6 +80,19 @@ const SignUp = () => {
                             />
                         </div>
                     </div>
+
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+                    <button type="submit" className="btn-primary">
+                        SIGN UP
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        Already have an account?{" "}
+                        <Link className="font-medium text-primary underline" to="/login">
+                        Login
+                        </Link>
+                    </p>
                 </form>
             </div>
         </AuthLayout>
